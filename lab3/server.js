c1b5e7ee83a52dbdd961f898a7725f7613c2a70a
ZubMaksym <zubmaxim171006@gmail.com>
2026-04-09 18:06:48 +0300

    add KABAN and swagger

--- lab3/server.js
+++ lab3/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const path = require('path');
+const swaggerUi = require('swagger-ui-express');
+const swaggerSpec = require('./config/swagger');
 const languageRoutes = require('./routes/languageRoutes');
 const dictionaryRoutes = require('./routes/dictionaryRoutes');
 const translationRoutes = require('./routes/translationRoutes');
@@ -15,6 +17,7 @@ app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use('/', languageRoutes);
 app.use('/', dictionaryRoutes);
 app.use('/', translationRoutes);
