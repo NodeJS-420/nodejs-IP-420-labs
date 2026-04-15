8cb1530420ac009b42278f2d8a6949263eb744b5
Bop-bili-bop <kisurin.kyrylo@lll.kpi.ua>
2026-04-15 23:52:31 +0300

    done

--- lab4/routes/wordRoutes.js
+++ lab4/routes/wordRoutes.js
@@ -3,8 +3,11 @@ const router = express.Router();
 const wordController = require('../controllers/wordController');
 
 router.get('/words', wordController.getWords.bind(wordController));
-router.get('/words/:id', wordController.getWordById.bind(wordController));
 router.post('/words', wordController.createWord.bind(wordController));
+router.get('/words/:id/edit', (req, res) => wordController.getEditPage(req, res));
+router.post('/words/:id/update', (req, res) => wordController.updateWord(req, res));
+router.post('/words/:id/delete', (req, res) => wordController.deleteWord(req, res));
+router.get('/words/:id', wordController.getWordById.bind(wordController));
 router.put('/words/:id', wordController.updateWord.bind(wordController));
 router.delete('/words/:id', wordController.deleteWord.bind(wordController));
 
